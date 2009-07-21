8af8b1f91709458aea73b0d83e2a0b0c2bffbab4
Sharoon Thomas <sharoonthomas@teagarden.in>
2009-07-21 09:52:40 +0530

    [INIT] Initial Release

---
+++ __terp__.py
@@ -0,0 +1,43 @@
+#########################################################################
+#Power Email is a module for Open ERP which enables it to send mails    #
+#to customers, suppliers etc. and also has a fiull fledged email client.#
+#########################################################################
+#   #####     #   #        # ####  ###     ###  #   #   ##  ###   #     #
+#   #   #   #  #   #      #  #     #  #    #    # # #  #  #  #    #     #
+#   ####    #   #   #    #   ###   ###     ###  #   #  #  #  #    #     #
+#   #        # #    # # #    #     # #     #    #   #  ####  #    #     #
+#   #         #     #  #     ####  #  #    ###  #   #  #  # ###   ####  #
+# Copyright (C) 2009  Sharoon Thomas                                    #
+#                                                                       #
+#This program is free software: you can redistribute it and/or modify   #
+#it under the terms of the GNU General Public License as published by   #
+#the Free Software Foundation, either version 3 of the License, or      #
+#(at your option) any later version.                                    #
+#                                                                       #
+#This program is distributed in the hope that it will be useful,        #
+#but WITHOUT ANY WARRANTY; without even the implied warranty of         #
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          #
+#GNU General Public License for more details.                           #
+#                                                                       #
+#You should have received a copy of the GNU General Public License      #
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.  #
+#########################################################################
+
+{
+    "name" : "Powerful Email capabilities for Open ERP",
+    "version" : "0.1.0",
+    "author" : "Sharoon Thomas, TL-Pragmatic",
+    "website" : "",
+    "category" : "Added functionality",
+    "depends" : ['base'],
+    "description": """
+    A module similar to the smtpclient and email_sale etc etc. But lot more powerful. Creates three user groups:1.Email Manager(obvious), 2.Email External(Send email to partners),3.Email Internal (mail to seniors etc). the module supports cc, bcc etc which the present smtp client does not. Most unique thing is you can create default settings for sale order, invoice, etc with default cc's,bcc's and default subject, report name and body. the subject, reportname and body takes placeholders which has over 12 functions eg. can get customer name with %(cust_name) etc etc.
+        
+    """,
+    "update_xml": ['PowerEmailGroup_data.xml',"poweremail_wizard.xml",'power_email_view.xml','poweremail_sale.xml','poweremail_inv.xml','poweremail_workflow.xml','poweremail_voucher.xml',"poweremail_demo.xml", ],
+    "demo_xml" : [],
+    "installable": True,
+    "active": False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
