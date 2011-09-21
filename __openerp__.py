042507aeca5731ce22d53d0c670037e24602c005
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2011-09-21 15:33:41 -0400

    Refactored __openerp__.py for version change and first step to refactoring

--- __openerp__.py
+++ __openerp__.py
@@ -1,12 +1,6 @@
 #########################################################################
-#Power Email is a module for Open ERP which enables it to send mails    #
-#to customers, suppliers etc. and also has a fiull fledged email client.#
-#########################################################################
-#   #####     #   #        # ####  ###     ###  #   #   ##  ###   #     #
-#   #   #   #  #   #      #  #     #  #    #    # # #  #  #  #    #     #
-#   ####    #   #   #    #   ###   ###     ###  #   #  #  #  #    #     #
-#   #        # #    # # #    #     # #     #    #   #  ####  #    #     #
-#   #         #     #  #     ####  #  #    ###  #   #  #  # ###   ####  #
+#                                                                       #
+# Copyright (C) 2010-2011 Openlabs Technologies & Consulting (P) LTD    #
 # Copyright (C) 2009  Sharoon Thomas                                    #
 #                                                                       #
 #This program is free software: you can redistribute it and/or modify   #
@@ -24,31 +18,38 @@
 #########################################################################
 
 {
-    "name" : "Powerful Email capabilities for Open ERP",
-    "version" : "0.7 RC",
+    "name" : "Poweremail",
+    "version" : "0.7",
     "author" : "Sharoon Thomas, Openlabs",
-    "website" : "http://openlabs.co.in/blog/post/poweremail/",
+    "website" : "http://openlabs.co.in/article/poweremail",
     "category" : "Added functionality",
-    "depends" : ['base'],
+    "depends" : [
+        'base',
+    ],
     "description": """
-    Power Email - extends the most Power ful open source ERP with email which powers the world today.
+Power Email - extends the most Power ful open source ERP with email 
+which powers the world today.
 
-    Features:
+Features:
 
-    1. Multiple Email Accounts
-    2. Company & Personal Email accounts
-    3. Security (In Progress)
-    4. Email Folders (Inbox.Outbox.Drafts etc)
-    5. Sending of Mails via SMTP (SMTP SSL also supported)
-    6. Reception of Mails (IMAP & POP3) With SSL & Folders for IMAP supported
-    7. Simple one point Template designer which automatically updates system. No extra config req.
-    8. Automatic Email feature on workflow stages
+1. Multiple Email Accounts
+2. Company & Personal Email accounts
+3. Security (In Progress)
+4. Email Folders (Inbox.Outbox.Drafts etc)
+5. Sending of Mails via SMTP (SMTP SSL also supported)
+6. Reception of Mails (IMAP & POP3) With SSL & Folders for IMAP supported
+7. Simple one point Template designer which automatically updates system. 
+    No extra config req.
+8. Automatic Email feature on workflow stages
 
-    NOTE: This is a beta release. Please update bugs at:
-    http://bugs.launchpad.net/poweremail/+filebug (or) https://bugs.launchpad.net/poweremail/+filebug
+NOTE: This is a beta release. Please update bugs at:
+
+https://github.com/openlabs/poweremail/issues/new
 
     """,
-    "init_xml": ['poweremail_scheduler_data.xml'],
+    "init_xml": [
+        'poweremail_scheduler_data.xml'
+    ],
     "update_xml": [
         'security/poweremail_security.xml',
         'security/ir.model.access.csv',
@@ -57,10 +58,8 @@
         'poweremail_template_view.xml',
         'poweremail_send_wizard.xml',
         'poweremail_mailbox_view.xml',
-        'poweremail_serveraction_view.xml'
+        'poweremail_serveraction_view.xml',
     ],
     "installable": True,
     "active": False,
 }
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
